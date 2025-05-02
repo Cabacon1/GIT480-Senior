8e1307c69a9358b647ebbe64a6dd856350dbde28
Cabacon1 <116121814+Cabacon1@users.noreply.github.com>
2025-05-02 13:31:38 -0400

    CSS Media for tablets

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ const products = [
     name: "Deluxe",
     description: "A dozen 3-4 cookies designed and decorated based on the theme of your choosing. Standard designs may include piping, simpler florals, hand drawn elements, simple painting, and larger lettering.",
     price: 72,
-    image: "images/Deluxe.jpg"
+    image: "images/Basketball.jpg"
   },
   {
     id: 3,
